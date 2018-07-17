f0ca5be58a52681bb7e70a46651fcb518bc0333a
chatterjeesurupa <chatterjee.su@husky.neu.edu>
2018-07-16 22:13:19 -0400

    changes in url

--- src/services/CourseService.js
+++ src/services/CourseService.js
@@ -1,6 +1,6 @@
 let _singleton = Symbol();
 
-const COURSE_API_URL ="https://course-mgmt-react-client-stc.herokuapp.com/api/course";
+const COURSE_API_URL ="https://course-management-stc.herokuapp.com/api/course";
 //const COURSE_API_URL ="http://localhost:8080/api/course";
 
 
