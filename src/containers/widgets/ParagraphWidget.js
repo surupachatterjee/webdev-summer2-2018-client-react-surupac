bf31cf416d4098a55f8617896d9673fe63c9751e
chatterjeesurupa <chatterjee.su@husky.neu.edu>
2018-08-01 18:52:50 -0400

    changes to paragraph widget

--- src/containers/widgets/ParagraphWidget.js
+++ src/containers/widgets/ParagraphWidget.js
@@ -1,34 +1,34 @@
 import React from 'react'
 
-const ParagraphWidget = ({widget, updateWidget,preview}) => {
+const ParagraphWidget = ({widget, updateWidget, preview}) => {
     let text;
     let pname;
     return (
         <div>
-
-            <h1>Paragraph Widget({widget.id}) - {widget.name}</h1>
             {!preview &&
             <div>
-            <label htmlFor="ptext">Paragraph Text</label>
-            <textarea className="form-control"
-                      placeholder="Paragraph Text"
-                      ref={node => text = node}
-                      onChange={() =>
-                      {
-                          widget.text = text.value;
-                          updateWidget(widget)
-                      }}
-                      id="ptext"/>
-            <label htmlFor="pname">Widget Name</label>
-            <input className="form-control"
-                   placeholder="Widget Name"
-                   ref={node =>pname=node}
-                   onChange={() => {
-                       widget.name=pname.value
-                       updateWidget(widget)
-                   }}
-                   id="pname"/>
-            <h4>Preview</h4>
+                <h1>Paragraph Widget</h1>
+                <label htmlFor="ptext">Paragraph Text</label>
+                <textarea className="form-control"
+                          placeholder="Paragraph Text"
+                          defaultValue={widget.text}
+                          ref={node => text = node}
+                          onChange={() => {
+                              widget.text = text.value;
+                              updateWidget(widget)
+                          }}
+                          id="ptext"/>
+                <label htmlFor="pname">Widget Name</label>
+                <input className="form-control"
+                       placeholder="Widget Name"
+                       defaultValue={widget.name}
+                       ref={node => pname = node}
+                       onChange={() => {
+                           widget.name = pname.value
+                           updateWidget(widget)
+                       }}
+                       id="pname"/>
+                <h4>Preview</h4>
             </div>}
             {widget.text}
         </div>
