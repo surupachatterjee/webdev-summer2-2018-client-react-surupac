1e3543786f73c8b5c0a7643ea839bd0197f794b6
chatterjeesurupa <chatterjee.su@husky.neu.edu>
2018-08-01 18:30:27 -0400

    changes to link widget

--- src/containers/widgets/LinkWidget.js
+++ src/containers/widgets/LinkWidget.js
@@ -1,50 +1,49 @@
 import React from 'react'
 
-const LinkWidget = ({widget,updateWidget,preview}) =>
-{
+const LinkWidget = ({widget, updateWidget, preview}) => {
     let linkurl;
     let linktext;
     let name;
-    return(
+    return (
         <div>
-            <h1>Link Widget({widget.id}) - {widget.name}</h1>
             {!preview &&
             <div>
-            <label htmlFor="linkurl">Link URL</label>
-            <input placeholder="Link URL"
-                   className="form-control"
-                   ref={node => linkurl=node}
-                   onChange={() =>
-                   {
-                       widget.href=linkurl.value
-                       updateWidget(widget)
-                   }}
+                <h1>Link Widget</h1>
+                <label htmlFor="linkurl">Link URL</label>
+                <input placeholder="Link URL"
+                       className="form-control"
+                       defaultValue={widget.href}
+                       ref={node => linkurl = node}
+                       onChange={() => {
+                           widget.href = linkurl.value
+                           updateWidget(widget)
+                       }}
 
-                    id="linkurl"/><br/>
+                       id="linkurl"/><br/>
 
-            <label htmlFor="linktext">Link Text</label>
-            <input placeholder="Link Text"
-                   className="form-control"
-                   ref={node => linktext=node}
-                   onChange={() =>
-                   {
-                       widget.text =linktext.value;
-                       updateWidget(widget)
+                <label htmlFor="linktext">Link Text</label>
+                <input placeholder="Link Text"
+                       defaultValue={widget.text}
+                       className="form-control"
+                       ref={node => linktext = node}
+                       onChange={() => {
+                           widget.text = linktext.value;
+                           updateWidget(widget)
 
-                   }}
-                   id="linktext"/><br/>
+                       }}
+                       id="linktext"/><br/>
 
-            <label htmlFor="wname">Widget Name</label>
-            <input placeholder="widget Name"
-                   ref={node =>name=node}
-                   onChange={() => {
-                       widget.name=name.value
-                       updateWidget(widget)
-                   }}
-                   className="form-control"
-                   id="wname"/><br/>
+                <label htmlFor="wname">Widget Name</label>
+                <input placeholder="widget Name"
+                       ref={node => name = node}
+                       onChange={() => {
+                           widget.name = name.value
+                           updateWidget(widget)
+                       }}
+                       className="form-control"
+                       id="wname"/><br/>
 
-            <h4>Preview</h4>
+                <h4>Preview</h4>
             </div>}
             <a href={widget.href}>{widget.text}</a>
 
